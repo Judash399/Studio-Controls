875c8d436b8211835e0fb0f9be7c124bc888e036
Judash399 <Judash399@gmail.com>
2025-10-20 13:51:46 -0600

    Updated main.lua

--- src/Main.server.lua
+++ src/Main.server.lua
@@ -1,1 +1,15 @@
-print("hello world!")
+--Services
+local RunService = game:GetService("RunService")
+
+if RunService:IsRunning() then
+    return --In a running game, do nothing.
+end
+
+--Modules
+local Packages = script.Parent.Packages
+
+--@type Fusion
+local Fusion = require(Packages.Fusion)
+
+--@type PluginEssentials
+local PluginEssentials = require(Packages.PluginEssentials)
