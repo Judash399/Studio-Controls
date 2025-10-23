a78b2ac9561dc1476919778f84ca419b8257c941
Judash399 <Judash399@gmail.com>
2025-10-23 15:07:42 -0600

    Add key utilities and object influence retrieval
    Introduces CombineKey and DesectKey functions for managing influence keys, and adds GetAllFromObject to retrieve all influences associated with a specific object's properties.

--- src/Controllers/ControllerUtils/InfluenceManager.lua
+++ src/Controllers/ControllerUtils/InfluenceManager.lua
@@ -3,27 +3,47 @@ local Fusion = require(script.Parent.Parent.Parent.Packages.fusion)
 local methods = {}
 methods.__index = methods
 
---Creates a link to a value.
-function methods:LinkToInfluence(ObjectType: string, ObjectId: string, propertyName: string, value: Fusion.Value<Fusion.Scope<any>, any>)
-    local scope: Fusion.Scope<any> = self._scope
-
-    local InfluenceIndex = nil
+local function CombineKey(ObjectType: string, ObjectId: string, propertyName: string)
+    local key = nil
     if ObjectId then
-        InfluenceIndex = ObjectType .. "." .. ObjectId .. "." .. propertyName
+        key = ObjectType .. "." .. ObjectId .. "." .. propertyName
     else
-        InfluenceIndex = ObjectType .. "." .. propertyName
+        key = ObjectType .. "." .. propertyName
     end
 
-    local value = self._influenceList[InfluenceIndex]
+    return key
+end
 
-    if not value then
-        return
+local function DesectKey(key): {ObjectType: string, ObjectId: string, propertyName: string}
+    local table = string.split(key, ".")
+
+    if #table == 3 then
+        return {
+            ObjectType = table[1],
+            ObjectId = table[2],
+            propertyName = table[3],
+        }
+    elseif #table == 2 then
+        return {
+            ObjectType = table[1],
+            propertyName = table[2],
+        }
+    else
+        error("DesectKey: invalid key format. Expected 'ObjectType.propertyName' or 'ObjectType.ObjectId.propertyName', got " .. #table .. " segment(s): '" .. key .. "'")
     end
+end
 
+--Returns a list of all the influences which value derives from the speified objects props. 
+function methods:GetAllFromObject(ObjectType: string, ObjectId: string): {[string]: string}
+    local matching = {}
 
-    value = scope:Computed(function(use, scope)
-        
-    end)
+    for i, influenceOutput in pairs(self._influenceList) do
+        local keyInfo = DesectKey(i)
+
+        if keyInfo.ObjectId == ObjectId and keyInfo.ObjectType == ObjectType then
+            matching[i] = influenceOutput
+        end
+    end
 end
 
 return function(scope: Fusion.Scope<any>, influenceList)
